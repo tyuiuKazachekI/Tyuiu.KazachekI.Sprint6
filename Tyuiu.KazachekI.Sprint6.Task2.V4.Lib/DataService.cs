a99dc5af272649b0f3e83f2897a45dd489ddd3f5
Казачек Иван <tyuiuKazachekI@outlook.com>
2025-12-12 17:46:26 +0500

    new interface

--- Tyuiu.KazachekI.Sprint6.Task2.V4.Lib/DataService.cs
+++ Tyuiu.KazachekI.Sprint6.Task2.V4.Lib/DataService.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.KazachekI.Sprint6.Task2.V4.Lib
 {
-    public class DataService
+    public class DataService : ISprint6Task2V4
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
