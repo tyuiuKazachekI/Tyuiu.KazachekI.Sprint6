9a738c445a4fdf0c297325f2befbe98fa48575dd
Казачек Иван <tyuiuKazachekI@outlook.com>
2025-12-12 17:23:00 +0500

    Task7

--- Tyuiu.KazachekI.Sprint6.Task7.V2.Lib/DataService.cs
+++ Tyuiu.KazachekI.Sprint6.Task7.V2.Lib/DataService.cs
@@ -8,9 +8,7 @@ namespace Tyuiu.KazachekI.Sprint6.Task7.V2.Lib
     {
         public int[,] GetMatrix(string path)
         {
-            if (!File.Exists(path))
-                throw new FileNotFoundException("Файл не найден", path);
-
+            // Чтение всех строк CSV
             string[] lines = File.ReadAllLines(path);
 
             int rows = lines.Length;
@@ -18,11 +16,26 @@ namespace Tyuiu.KazachekI.Sprint6.Task7.V2.Lib
 
             int[,] matrix = new int[rows, cols];
 
+            // Заполняем матрицу из CSV
             for (int i = 0; i < rows; i++)
             {
                 string[] parts = lines[i].Split(';');
+
                 for (int j = 0; j < cols; j++)
+                {
                     matrix[i, j] = Convert.ToInt32(parts[j]);
+                }
+            }
+
+            // === ГЛАВНАЯ ЛОГИКА ===
+            // ВО ВТОРОЙ строке (индекс 1)
+            // заменить ЧЁТНЫЕ числа на 555
+            for (int j = 0; j < cols; j++)
+            {
+                if (matrix[1, j] % 2 == 0) // чётное
+                {
+                    matrix[1, j] = 555;
+                }
             }
 
             return matrix;
