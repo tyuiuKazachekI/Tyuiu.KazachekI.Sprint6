dfc53f5b8438b8f4618c6d8eddf1be5026420739
Казачек Иван <tyuiuKazachekI@outlook.com>
2025-12-12 17:40:07 +0500

    Task2

--- Tyuiu.KazachekI.Sprint6.Task2.V4.Lib/DataService.cs
+++ Tyuiu.KazachekI.Sprint6.Task2.V4.Lib/DataService.cs
@@ -10,19 +10,23 @@ namespace Tyuiu.KazachekI.Sprint6.Task2.V4.Lib
             int length = stopValue - startValue + 1;
             double[] result = new double[length];
 
-            for (int i = 0; i < length; i++)
+            int index = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
             {
-                double x = startValue + i;
-                double denominator = Math.Sin(x + 2) + 2 * x + 2;
+                double denominator = Math.Sin(x) + 2;
 
                 if (Math.Abs(denominator) < 1e-10)
                 {
-                    result[i] = 0;  // защита от деления на 0
+                    result[index] = 0;
                 }
                 else
                 {
-                    result[i] = Math.Round((5 * x + 2.5) / denominator, 2);
+                    double value = ((5 * x + 2.5) / denominator) + (2 * x + 2);
+                    result[index] = Math.Round(value, 2);
                 }
+
+                index++;
             }
 
             return result;
